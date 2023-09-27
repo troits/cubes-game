00bc71d3f847d8a033f4001d337b896458550cdb
Ilya Troitskiy <troits@gmail.com>
2023-09-27 16:27:39 +0300

    add cube module

---
+++ src/app/cube.module.ts
@@ -0,0 +1,8 @@
+import { NgModule } from "@angular/core";
+import { CubeComponent } from "src/app/cube/cube.component";
+
+@NgModule({
+    declarations: [CubeComponent],
+    exports: [CubeComponent],
+})
+export class CubeModule {}
