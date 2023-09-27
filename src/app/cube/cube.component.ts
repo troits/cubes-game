5f3f1b770082256c49b6a0f73ab9434568314b7d
Ilya Troitskiy <troits@gmail.com>
2023-09-27 14:06:30 +0300

    vscode settings and cube component init

---
+++ src/app/cube/cube.component.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: "cube",
+    templateUrl: "./cube.component.html",
+    styleUrls: ["./cube.component.scss"],
+})
+export class CubeComponent {}
