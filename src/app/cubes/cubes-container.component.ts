23a03f1b6cf27f7be07ee0582baad6b3c8d99004
Ilya Troitskiy <troits@gmail.com>
2024-03-14 23:04:46 +0300

    sum of values added

--- src/app/cubes/cubes-container.component.ts
+++ src/app/cubes/cubes-container.component.ts
@@ -1,6 +1,6 @@
 import { CommonModule } from "@angular/common";
 import { Component, ElementRef, HostListener, OnInit } from "@angular/core";
-import { BehaviorSubject } from "rxjs";
+import { BehaviorSubject, combineLatest, map } from "rxjs";
 import { CubeComponent } from "src/app/cube/cube.component";
 
 @Component({
@@ -11,9 +11,13 @@ import { CubeComponent } from "src/app/cube/cube.component";
     styleUrls: ["./cubes-container.component.scss"],
 })
 export class CubesContainerComponent implements OnInit {
-    protected cube1Vals = new BehaviorSubject<number>(1);
-    protected cube2Vals = new BehaviorSubject<number>(1);
-    protected cube3Vals = new BehaviorSubject<number>(1);
+    protected cube1Vals$ = new BehaviorSubject<number>(1);
+    protected cube2Vals$ = new BehaviorSubject<number>(1);
+    protected cube3Vals$ = new BehaviorSubject<number>(1);
+
+    protected sumVals$ = combineLatest([this.cube1Vals$, this.cube2Vals$, this.cube3Vals$]).pipe(
+        map(([cube1Val, cube2Val, cube3Val]) => cube1Val + cube2Val + cube3Val),
+    );
 
     @HostListener("document:click")
     public clickout(): void {
@@ -25,9 +29,9 @@ export class CubesContainerComponent implements OnInit {
     }
 
     private retry(): void {
-        this.cube1Vals.next(getRandomInt(1, 6));
-        this.cube2Vals.next(getRandomInt(1, 6));
-        this.cube3Vals.next(getRandomInt(1, 6));
+        this.cube1Vals$.next(getRandomInt(1, 6));
+        this.cube2Vals$.next(getRandomInt(1, 6));
+        this.cube3Vals$.next(getRandomInt(1, 6));
     }
 }
 
